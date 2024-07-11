85506695f286ea57def27ff7ed54ac27a6448e10
Eli Maxwell-Aidam <emaxwellaidam@gmail.com>
2024-07-11 16:32:34 +0000

    Overly Complete redo of backend logic

--- routes/sslRoutes.js
+++ routes/sslRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
-const {certificate, verifyDomain} = require("../controllers/sslController")
+const {certificate} = require("../controllers/sslController")
 
 router.post("/generate-cert",certificate)
-router.post("/verify-domain", verifyDomain);
+// router.post("/verify-domain", verifyDomain);
 
 module.exports = router
