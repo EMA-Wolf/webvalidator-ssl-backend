a5ff12f45fe60027879e3e91feb6d27a0cc56a14
Eli Maxwell-Aidam <emaxwellaidam@gmail.com>
2024-07-18 11:06:52 +0000

    Addition of processing multiple domain names when trying to generate SSL Certificate

--- utils/generateCertificate.js
+++ utils/generateCertificate.js
@@ -18,11 +18,24 @@ const initializeClient = async (email) => {
     }
 };
 
+const processDomains = (domain) => {
+    return domain.split(',')
+                 .map(d => d.trim())
+                 .map(d => d.replace(/^www\./, ''));
+};
+
 const generateCertificate = async(domain,email) =>{
     await initializeClient(email);
 
+    // const domainNames = domain.split(',').map(d => d.trim())
+    const domainNames = processDomains(domain)
+
+    // const order = await client.createOrder({
+    //     identifiers: [{ type: 'dns', value: domain }]
+    // });
+
     const order = await client.createOrder({
-        identifiers: [{ type: 'dns', value: domain }]
+        identifiers: domainNames.map(domain => ({ type: 'dns', value: domain }))
     });
 
     const [authorization] = await client.getAuthorizations(order);
@@ -45,15 +58,20 @@ const generateCertificate = async(domain,email) =>{
 
 const verifyChallengeAndGetCertificate = async ({ order, authorization, httpChallenge},domain)=>{
 
-  
+    // const domainNames = domain.split(',').map(d => d.trim())
+    const domainNames = processDomains(domain)
+
     await client.verifyChallenge(authorization, httpChallenge);
     await client.completeChallenge(httpChallenge);
     await client.waitForValidStatus(order);
   
 
-
+    // const [key, csr] = await acme.crypto.createCsr({
+    //     altNames:[domain],
+    // });
+    
     const [key, csr] = await acme.crypto.createCsr({
-        altNames:[domain],
+        altNames:domainNames,
     });
 
    const finalized = await client.finalizeOrder(order, csr)
